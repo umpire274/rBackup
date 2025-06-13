99423453938d67f2a5f87bbcd8d844925709a89b
Alessandro Maestri <a.maestri@almaviva.it>
2025-06-13 13:41:12 +0200

    Fixed problem for compiling Windows subsystem

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 #[cfg(target_os = "windows")]
 fn main() {
-    embed_resource::compile("rbackup.rc").unwrap();
+    embed_resource::compile("rbackup.rc", std::iter::empty::<&str>());
 }
 
 #[cfg(not(target_os = "windows"))]
 fn main() {
-    // No resource compilation needed on non-Windows platforms
+    // Niente da fare su macOS/Linux
 }
