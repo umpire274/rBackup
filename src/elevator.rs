8c1ee9c1fdaee51f825f654b4a7bfea42da758f0
Alessandro Maestri <umpire274@gmail.com>
2025-06-28 12:30:21 +0200

    Fixed errors for CI compilation on Windows

--- src/elevator.rs
+++ src/elevator.rs
@@ -1,42 +1,40 @@
 #![cfg(windows)]
 
+use windows::core::Result;
 use windows::Win32::{
-    Foundation::{CloseHandle, HANDLE},
+    Foundation::CloseHandle,
     Security::{
-        GetTokenInformation, TokenElevationType, TokenElevationTypeDefault, TokenElevationTypeFull,
-        TOKEN_ELEVATION_TYPE, TOKEN_INFORMATION_CLASS, TOKEN_QUERY,
+        GetTokenInformation, TokenElevation, TOKEN_ELEVATION, TOKEN_INFORMATION_CLASS, TOKEN_QUERY,
     },
     System::Threading::{GetCurrentProcess, OpenProcessToken},
 };
 
 pub fn is_running_as_admin() -> bool {
     unsafe {
-        let mut token_handle = HANDLE::default();
+        let mut token_handle = std::mem::zeroed();
 
-        // Apri il token di accesso al processo corrente
-        let result = OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &mut token_handle);
-        if result.0 == 0 {
+        // Apri il token del processo corrente
+        if let Err(_) = OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &mut token_handle) {
             return false;
         }
 
-        let mut elevation_type = TOKEN_ELEVATION_TYPE::default();
-        let mut returned_length = std::mem::size_of::<TOKEN_ELEVATION_TYPE>() as u32;
+        let mut elevation = TOKEN_ELEVATION::default();
+        let mut size = std::mem::size_of::<TOKEN_ELEVATION>() as u32;
 
         let result = GetTokenInformation(
             token_handle,
-            TOKEN_INFORMATION_CLASS(18), // TokenElevationType
-            Some(&mut elevation_type as *mut _ as *mut _),
-            returned_length,
-            &mut returned_length,
+            TOKEN_INFORMATION_CLASS::TokenElevation,
+            Some(&mut elevation as *mut _ as *mut _),
+            size,
+            &mut size,
         );
 
         CloseHandle(token_handle);
 
-        if result.0 == 0 {
-            return false;
+        match result {
+            Ok(_) => elevation.TokenIsElevated != 0,
+            Err(_) => false,
         }
-
-        elevation_type == TokenElevationTypeFull
     }
 }
 
